b89a87b73731ce94955def44dc0f6a505371debd
Eldar Pashazade <eldarlrd@protonmail.com>
2022-11-06 21:58:34 +0400

    Update styles.ts

--- src/screens/profile/components/ProfileOptions/styles.ts
+++ src/screens/profile/components/ProfileOptions/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 34,
+    marginVertical: 20,
     marginHorizontal: 10,
     overflow: 'hidden',
     backgroundColor: '#FFF',
